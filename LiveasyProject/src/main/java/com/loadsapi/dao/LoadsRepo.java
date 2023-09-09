98ca7d8b3b49d40bf2a5666c91f454e218fd9ef2
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-09-09 23:55:40 +0530

    Update LoadsRepo.java

--- LiveasyProject/src/main/java/com/loadsapi/dao/LoadsRepo.java
+++ LiveasyProject/src/main/java/com/loadsapi/dao/LoadsRepo.java
@@ -3,6 +3,6 @@ package com.loadsapi.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.loadsapi.entites.Payloads;
 
-public interface LoadsRepo extends JpaRepository<Payloads,Integer> {
+public interface LoadsRepo extends JpaRepository< Payloads,Integer > {
 
 }
